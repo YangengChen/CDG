bc64da341c628df625ccfd4c46bfe7d3905fee00
root <root@Micah.localdomain>
2018-04-17 00:22:28 -0400

    Register User Persist [no checking]

---
+++ client/src/environments/endpoints.ts
@@ -0,0 +1,5 @@
+export const endpoints = {
+  LOGIN_ENDPOINT	       	: "http://localhost:8080" + "/api/user/login",
+  LOGOUT_ENDPOINT	       	: "http://localhost:8080" + "/api/user/logout",
+  REGISTER_ENDPOINT       	: "http://localhost:8080" + "/api/user/register",
+}
